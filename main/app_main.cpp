f8a116dda1d637c107227beb356f5009e0e5e12d
nix <609610903@qq.com>
2025-03-19 14:37:12 +0800

    new file:   components/mooncake 	modified:   main/CMakeLists.txt 	modified:   main/app_main.cpp

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -28,7 +28,7 @@
 #include "esp_lcd_touch_ft5x06.h"
 #include "esp_codec_dev_defaults.h"
 #include "esp_camera.h"
-#include "SensorLTR553.hpp"
+// #include "SensorLTR553.hpp"
 //*Device
 #include "bsp/esp-bsp.h"
 // #include "M5CoreS3.hpp"
@@ -41,44 +41,6 @@
 
 using namespace std;
 using namespace mooncake;
-// using namespace dl;
-
-class M5CoreS3
-{
-public:
-
-	static M5CoreS3& getInstance() 
-	{
-		static M5CoreS3 incetance;
-		return incetance;
-	}
-
-	M5CoreS3(const M5CoreS3&) = delete;
-	M5CoreS3& operator = (const M5CoreS3&) = delete;
-
-	esp_err_t i2cInit() { return bsp_i2c_init(); }
-	esp_err_t i2cDeinit() { return bsp_i2c_deinit(); }
-	lv_disp_t* displayInit() { return bsp_display_start(); }
-	esp_err_t displayOn() { return bsp_display_backlight_on(); }
-	esp_err_t displayOff() { return bsp_display_backlight_off(); }
-	// esp_err_t cameraInit(framesize_t framesize) { return bsp_camera_init(framesize); }
-	// esp_err_t cameraDeinit() { return bsp_camera_deinit(); }
-
-private:
-
-	M5CoreS3()
-	{
-		
-	}
-
-	~M5CoreS3()
-	{
-
-	}
-
-};
-
-M5CoreS3& core = M5CoreS3::getInstance();	
 
 extern "C" void app_main(void)
 {
